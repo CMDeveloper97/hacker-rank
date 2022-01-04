9982eab1d5524045ac8a52aa8189900698295cd5
Carlos Mauricio <cmdeveloper97@gmail.com>
2022-01-04 01:32:26 -0700

    Add index to components folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { Header } from "./components/Header";
-import { MainContainer } from "./components/containers/MainContainer";
-import { FavsSelector } from "./components/FavsSelector";
-import { Filter } from './components/Filter';
-import { Pagination } from './components/Pagination';
-import { PostsContainer } from "./components/containers/PostsContainer";
+import {
+  Header,
+  MainContainer,
+  FavsSelector,
+  Filter,
+  Pagination,
+  PostsContainer,
+} from "./components";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <MainContainer>
-        <FavsSelector/>
-        <Filter/>
-        <PostsContainer/>
-        <Pagination/>
+        <FavsSelector />
+        <Filter />
+        <PostsContainer />
+        <Pagination />
       </MainContainer>
     </div>
   );
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export * from './FavsSelector';
+export * from './Filter';
+export * from './Header';
+export * from './Pagination';
+export * from './Post';
+export * from './containers/MainContainer';
+export * from './containers/PostsContainer'; 
